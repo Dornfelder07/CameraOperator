5889ad5a809a5b825d2cb21ffbc0337dd785e6bf
Dornfelder07 <maciejanglaart@gmail.com>
2020-05-26 19:49:58 +0200

    VideoStream class code cleanup

--- VideoStream.py
+++ VideoStream.py
@@ -2,7 +2,6 @@ import cv2
 from threading import Thread
 
 class VideoStream:
-    """Camera object"""
     def __init__(self, resolution=(640,480),framerate=10,PiOrUSB=1,src=0):
         self.PiOrUSB = PiOrUSB
         self.stopped = False
