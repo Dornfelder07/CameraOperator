575ff71af8921d0797851baa6599bfb432c39000
Dornfelder07 <maciejanglaart@gmail.com>
2020-05-26 18:37:04 +0200

    Camera class default settings

---
+++ VideoStream.py
@@ -0,0 +1,12 @@
+import cv2
+from threading import Thread
+
+class VideoStream:
+    """Camera object"""
+    def __init__(self, resolution=(640,480),framerate=10,PiOrUSB=1,src=0):
+        self.PiOrUSB = PiOrUSB
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
