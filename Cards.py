94b9b63b1f9541becbf0667aceca2282ee5e7dd7
unknown <arkdokowicz@gmail.com>
2020-05-28 22:06:28 +0200

    cards query done

--- Cards.py
+++ Cards.py
@@ -17,4 +17,53 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class Query_card:
     def __init__(self):
-        self.width, self.height = 0, 0  # wxh karty
+        self.contour = []  # Contour of card
+        self.width, self.height = 0, 0  # Width and height of card
+        self.corner_pts = []  # Corner points of card
+        self.center = []  # Center point of card
+        self.warp = []  # 200x300, flattened, grayed, blurred image
+        self.rank_img = []  # Thresholded, sized image of card's rank
+        self.suit_img = []  # Thresholded, sized image of card's suit
+        self.best_rank_match = "Unknown"  # Best matched rank
+        self.best_suit_match = "Unknown"  # Best matched suit
+        self.rank_diff = 0  # Difference between rank image and best matched train rank image
+        self.suit_diff = 0  # Difference between suit image and best matched train suit image
+
+
+class Train_ranks:
+    def __init__(self):
+        self.img = []  # Thresholded, sized rank image loaded from hard drive
+        self.name = "Placeholder"
+
+
+class Train_suits:
+    def __init__(self):
+        self.img = []  # Thresholded, sized suit image loaded from hard drive
+        self.name = "Placeholder"
+
+def load_ranks(filepath):
+    train_ranks = []
+    i = 0
+
+    for Rank in ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+                 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']:
+        train_ranks.append(Train_ranks())
+        train_ranks[i].name = Rank
+        filename = Rank + '.jpg'
+        train_ranks[i].img = cv2.imread(filepath + filename, cv2.IMREAD_GRAYSCALE)
+        i = i + 1
+
+    return train_ranks
+
+def load_suits(filepath):
+    train_suits = []
+    i = 0
+
+    for Suit in ['Spades', 'Diamonds', 'Clubs', 'Hearts']:
+        train_suits.append(Train_suits())
+        train_suits[i].name = Suit
+        filename = Suit + '.jpg'
+        train_suits[i].img = cv2.imread(filepath + filename, cv2.IMREAD_GRAYSCALE)
+        i = i + 1
+
+    return train_suits
